8faf1bf550fd6871d0e54180efe53175b8cae93c
odkq <pablo@odkq.com>
2014-03-14 01:12:13 +0100

    Added placeholders for key handlers

--- 2048.py
+++ 2048.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import curses
+import sys
 
 def draw_board(stdscr, board):
     for i, x, y in map((lambda i: [i, i % 3, i / 3]), range(9)):
@@ -20,15 +21,28 @@ def move_up(board):
 def move_down(board):
     pass
 
+def exit(board):
+    sys.exit(0)
+
 def curses_main(stdscr):
     board = [ 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    keys = { curses.KEY_UP: move_up,
+             curses.KEY_DOWN: move_down,
+             curses.KEY_LEFT: move_left,
+             curses.KEY_RIGHT: move_right,
+             113: exit }
     for y in range(7):
         if y % 2:
             stdscr.addstr(y, 0, "|      |      |      |")
         else:
             stdscr.addstr(y, 0, "+------+------+------+")
+        stdscr.addstr(9, 0, "Use cursor keys to move, q to exit")
+
     while True:
         draw_board(stdscr, board)
-        stdscr.getch()
+        try:
+            keys[stdscr.getch()](board)
+        except KeyError:
+            pass
 
 curses.wrapper(curses_main)
