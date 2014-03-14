6f3724424b6db289eda8d4ceeb91a9270ca1fb80
odkq <pablo@odkq.com>
2014-03-14 01:01:32 +0100

    tunning

--- 2048.py
+++ 2048.py
@@ -1,13 +1,34 @@
 #!/usr/bin/env python
 import curses
 
+def draw_board(stdscr, board):
+    for i, x, y in map((lambda i: [i, i % 3, i / 3]), range(9)):
+        value = str(board[i]).center(6)
+        px = (x * 6) + x + 1
+        py = (y * 2) + 1
+        stdscr.addstr(py, px, value)
+
+def move_right(board):
+    pass
+
+def move_left(board):
+    pass
+
+def move_up(board):
+    pass
+
+def move_down(board):
+    pass
+
 def curses_main(stdscr):
+    board = [ 0, 0, 0, 0, 0, 0, 0, 0, 0]
     for y in range(7):
         if y % 2:
             stdscr.addstr(y, 0, "|      |      |      |")
         else:
             stdscr.addstr(y, 0, "+------+------+------+")
-    stdscr.getch()
+    while True:
+        draw_board(stdscr, board)
+        stdscr.getch()
 
 curses.wrapper(curses_main)
-
